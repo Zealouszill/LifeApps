b26a99a7c2a6b48654ef04acd4f9e9a13ba4dc34
Zealouszill <rubix.this@gmail.com>
2019-01-06 21:50:45 -0700

    Got a basic timer application to work. Will display to debug a Hello world, every 5 seconds.

---
+++ LifeApps/LifeApps/ViewModelLocater.cs
@@ -0,0 +1,24 @@
+﻿using SharedLogic;
+
+namespace LifeApps
+{
+    public class ViewModelLocator
+    {
+        
+        public ViewModelLocator()
+        {
+            MainViewModel mainView = new MainViewModel();
+
+            //IDataStorage testStorage = new SqliteDataStore();
+            //PotentialRepository testRepo = new PotentialRepository(testStorage);
+            
+            //Main = new MainViewModel(testRepo);
+
+        }
+
+
+        public MainViewModel Main { get; }
+
+
+    }
+}
---
+++ SharedLogic/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
+using System.Diagnostics;
+
+namespace SharedLogic
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        
+
+
+        public MainViewModel()
+        {
+            Timer aTimer = new Timer();
+
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+        }
+
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Debug.WriteLine("Hello World!");
+        }
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+    }
+}
